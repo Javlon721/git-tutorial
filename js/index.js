5b4c3f8e21d5654ff3644414e121f7c89a66f5ad
Javlon Talipov <javlonax@icloud.com>
2024-03-21 16:34:35 +0500

    add decrement via vscode

--- js/index.js
+++ js/index.js
@@ -6,4 +6,13 @@ function increment(el) {
   });
 }
 
+function decrement(el) {
+  const btn = document.querySelector(el);
+  const incrementEl = document.querySelector('.counter__number');
+  btn.addEventListener("click", () => {
+    incrementEl.innerHTML = +incrementEl.innerHTML - 1;
+  });
+}
+
+decrement('.counter__btn_decrement')
 increment('.counter__btn_increment')
