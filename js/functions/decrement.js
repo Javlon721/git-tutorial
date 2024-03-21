d21ab109d8b1acf99494245f2c57a457badd0278
Javlon Talipov <javlonax@icloud.com>
2024-03-21 22:46:16 +0500

    remove lines

--- js/functions/decrement.js
+++ js/functions/decrement.js
@@ -1,9 +1,6 @@
 function decrement(el) {
    const incrementEl = document.querySelector('.counter__number');
   const btn = document.querySelector(el);
-
-
-  
   btn.addEventListener("click", () => {
     incrementEl.innerHTML = +incrementEl.innerHTML - 1;
   });
