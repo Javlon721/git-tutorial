db90beb554b05b33eaabbaca2d4c8a340d802b25
Javlon Talipov <javlonax@icloud.com>
2024-03-21 22:13:56 +0500

    remove unnesesary stuff from increment.js
    i decided to write here
    so what gonne be then lets see

--- js/functions/decrement.js
+++ js/functions/decrement.js
@@ -1,8 +1,10 @@
 function decrement(el) {
-   const incrementEl1 = document.querySelector('.counter__number');
+   const incrementEl = document.querySelector('.counter__number');
   const btn = document.querySelector(el);
-  const incrementEl2 = document.querySelector('.counter__number');
+
+
+  
   btn.addEventListener("click", () => {
-    incrementEl1.innerHTML = +incrementEl2.innerHTML - 1;
+    incrementEl.innerHTML = +incrementEl.innerHTML - 1;
   });
 }
