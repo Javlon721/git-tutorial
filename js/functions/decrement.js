6990da1b11a51514cfdb7222131477edc7520acc
Javlon Talipov <javlonax@icloud.com>
2024-03-21 21:39:46 +0500

    add new functions
    those functions are for form handlers and
    increment and decrement el on the page

---
+++ js/functions/decrement.js
@@ -0,0 +1,7 @@
+function decrement(el) {
+  const btn = document.querySelector(el);
+  const incrementEl = document.querySelector('.counter__number');
+  btn.addEventListener("click", () => {
+    incrementEl.innerHTML = +incrementEl.innerHTML - 1;
+  });
+}
