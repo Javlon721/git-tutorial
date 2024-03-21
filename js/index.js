ff0c3a0d4f678d64fd83417fb5546314e64ca7c5
Javlon Talipov <88850773+Javlon721@users.noreply.github.com>
2024-03-21 16:33:40 +0500

    add decrement via github

--- js/index.js
+++ js/index.js
@@ -7,3 +7,13 @@ function increment(el) {
 }
 
 increment('.counter__btn_increment')
+
+function decrement(el) {
+  const btn = document.querySelector(el);
+  const incrementEl = document.querySelector('.counter__number');
+  btn.addEventListener("click", () => {
+    incrementEl.innerHTML = +incrementEl.innerHTML - 1;
+  });
+}
+
+decrement('.counter__btn_decrement')
