6c8aceb1f32ddfda053b03377ab1ce21e72ea564
Javlon Talipov <javlonax@icloud.com>
2024-03-21 16:26:00 +0500

    add increment function

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function increment(el) {
+  const btn = document.querySelector(el);
+  const incrementEl = document.querySelector('.counter__number');
+  btn.addEventListener("click", () => {
+    incrementEl.innerHTML = +incrementEl.innerHTML + 1;
+  });
+}
+
+increment('.counter__btn_increment')
