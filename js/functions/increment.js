5687e94b5f0c8d521f71a09a53b800045ab416ea
Javlon Talipov <javlon.talipov13@mail.ru>
2024-03-25 17:36:07 +0500

    remove all unnessesary log from increment.js

--- js/functions/increment.js
+++ js/functions/increment.js
@@ -2,9 +2,6 @@ function increment(el) {
   const btn = document.querySelector(el);
   const incrementEl = document.querySelector('.counter__number');
   btn.addEventListener("click", () => {
-    console.log(true)
     incrementEl.innerHTML = +incrementEl.innerHTML + 1;
-    console.log(window.innerWidth)
-     console.log(window.innerHeight)
   });
 }
