a9a565969fcc1e28a1ab5b97210a28bb87af9344
alladdin.work <alladdin.work@gmail.com>
2024-04-03 17:12:01 +0500

    delete increment and body dark

--- js/index.js
+++ js/index.js
@@ -1,10 +1,4 @@
-function increment(el) {
-  const btn = document.querySelector(el);
-  const incrementEl = document.querySelector('.counter__number');
-  btn.addEventListener("click", () => {
-    incrementEl.innerHTML = +incrementEl.innerHTML + 1;
-  });
-}
+
 
 function decrement(el) {
   const btn = document.querySelector(el);
@@ -15,4 +9,3 @@ function decrement(el) {
 }
 
 decrement('.counter__btn_decrement')
-increment('.counter__btn_increment')
