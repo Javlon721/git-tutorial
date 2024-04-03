31eb62af9a61722d7f3b0a3be30f4a6d71b3c88a
new name <followmoon98@gmail.com>
2024-04-03 17:07:35 +0500

    del

--- js/index.js
+++ js/index.js
@@ -8,12 +8,6 @@ function increment(el) {
 
 increment('.counter__btn_increment')
 
-function decrement(el) {
-  const btn = document.querySelector(el);
-  const incrementEl = document.querySelector('.counter__number');
-  btn.addEventListener("click", () => {
-    incrementEl.innerHTML = +incrementEl.innerHTML - 1;
-  });
-}
+KU
 
 decrement('.counter__btn_decrement')
