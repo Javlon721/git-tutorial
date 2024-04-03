34725e67603feb2f1a3485eb1e439b4f09168813
Javlon Talipov <javlon.talipov13@mail.ru>
2024-04-03 17:03:48 +0500

    remove increment javlon

--- js/index.js
+++ js/index.js
@@ -1,10 +1,4 @@
-function increment(el) {
-  const btn = document.querySelector(el);
-  const incrementEl = document.querySelector('.counter__number');
-  btn.addEventListener("click", () => {
-    incrementEl.innerHTML = +incrementEl.innerHTML + 1;
-  });
-}
+
 
 increment('.counter__btn_increment')
 
